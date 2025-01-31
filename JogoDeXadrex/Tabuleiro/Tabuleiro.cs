4efcba1fc497f6122478b8fdcfd21df258e580f0
Jose Fabio <fabio.7.45@hotmail.com>
2025-01-30 23:31:38 -0300

    Tratando erros com exceção personalizadas

--- JogoDeXadrex/Tabuleiro/Tabuleiro.cs
+++ JogoDeXadrex/Tabuleiro/Tabuleiro.cs
@@ -4,7 +4,7 @@ using JogoDeXadrez.Tabuleiro;
 
 namespace JogoDeXadrez.tabuleiro
 {
-     class Tabuleiro
+    class Tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
@@ -13,7 +13,7 @@ namespace JogoDeXadrez.tabuleiro
 
         public Tabuleiro(int linhas, int colunas)
         {
-           this.Linhas = linhas;
+            this.Linhas = linhas;
             this.Colunas = colunas;
             pecas = new Peca[Linhas, Colunas];
         }
@@ -22,10 +22,49 @@ namespace JogoDeXadrez.tabuleiro
         {
             return pecas[linha, coluna];
         }
+
+        // METODO SE EXISTE UMA PEÇA NUMA DADA POSIÇÃO
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+
+        }
+
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        // TESTAR POSIÇAÕ
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= Linhas || pos.coluna < 0 || pos.coluna > Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // VALIDAR POSIÇÃO
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
         }
     }
 }
---
+++ JogoDeXadrex/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace JogoDeXadrez.tabuleiro
+{
+     class TabuleiroException : Exception
+    {
+public TabuleiroException(string message): base (message) 
+        {
+        }
+    }
+}
